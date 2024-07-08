3ec2deaa9866002653242188c08a4cece5451ef1
Saad Tahir <saadtahir018@gmail.com>
2024-07-09 02:03:31 +0500

    use .env to hide database details

--- index.js
+++ index.js
@@ -28,11 +28,11 @@ app.use(
   );
 
 const db = new pg.Client({
-    user: "postgres",
-    host: "localhost",
-    database: "travel",
-    password: process.env.DB_PASS,
-    port: 5432,
+    user: process.env.PG_USER,
+    host: process.env.PG_HOST,
+    database: process.env.PG_DATABASE,
+    password: process.env.PG_PASSWORD,
+    port: process.env.PG_PORT,
 });
 db.connect();
 
